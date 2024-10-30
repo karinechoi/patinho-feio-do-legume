7db13b7135c8ffe3b215ce028c16544a75f1f424
alineyHiga <alineyHiga>
2024-10-30 00:15:11 -0300

    try adjust blob storage

--- main/storage/vercelStorage.py
+++ main/storage/vercelStorage.py
@@ -1,13 +1,15 @@
-# main/storage_backends/vercel_storage.py
-
 import requests
 from django.core.files.storage import Storage
 from django.conf import settings
 
 class VercelBlobStorage(Storage):
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.base_url = getattr(settings, 'VERCEL_BLOB_BASE_URL', None)
-        self.token = settings.VERCEL_BLOB_TOKEN
+        self.token = getattr(settings, 'VERCEL_BLOB_TOKEN', None)
+        
+        if not self.base_url or not self.token:
+            raise ValueError("VERCEL_BLOB_BASE_URL and VERCEL_BLOB_TOKEN must be set in settings")
 
     def _save(self, name, content):
         url = f"{self.base_url}/upload"
@@ -19,11 +21,11 @@ class VercelBlobStorage(Storage):
         if response.status_code == 200:
             return response.json().get('url')
         else:
-            raise Exception("Failed to upload file to Vercel Blob Storage")
+            # Adicione detalhes do erro na exceção
+            raise Exception(f"Failed to upload file to Vercel Blob Storage: {response.status_code} - {response.text}")
 
     def url(self, name):
         return f"{self.base_url}/{name}"
     
     def exists(self, name):
-        # Implemente se precisar verificar se o arquivo já existe (opcional)
         return False
