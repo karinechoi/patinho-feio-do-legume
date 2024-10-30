f0c9ae1bc37c66cda5912ee1ecd847ac56d1e78b
alineyHiga <alineyHiga>
2024-10-29 23:35:53 -0300

    add blob could storage

--- main/models.py
+++ main/models.py
@@ -1,11 +1,13 @@
 # main/models.py
 from django.db import models
 
+from produtoWebserver.settings import DEFAULT_FILE_STORAGE
+
 class Produto(models.Model):
     id_produto = models.AutoField(primary_key=True)
     nome = models.CharField(max_length=200)
     descricao = models.TextField()
-    foto = models.ImageField(upload_to='fotos_produtos/')  # Requer configuração do servidor de mídia
+    foto = models.ImageField(upload_to= DEFAULT_FILE_STORAGE)  # Requer configuração do servidor de mídia
     quantidade = models.DecimalField(max_digits=10, decimal_places=2)
     id_agricultor = models.IntegerField()
     classificacao = models.DecimalField(max_digits=3, decimal_places=1)
