53123c9576af670385a09669d50a8890528bdef9
alineyHiga <alineyHiga>
2024-10-30 07:39:22 -0300

    fix typerro

--- main/views/GetProdutoDisponivelView.py
+++ main/views/GetProdutoDisponivelView.py
@@ -4,7 +4,7 @@ from main.models import Produto
 from main.serializers import ProdutoSerializer
 
 @api_view(['GET'])
-def buscar_produtos_disponiveis():
+def buscar_produtos_disponiveis(request):
     produtos = Produto.objects.filter(quantidade__gt=0)  # Produtos com quantidade > 0 (disponíveis)
     serializer = ProdutoSerializer(produtos, many=True)
     return Response(serializer.data)
